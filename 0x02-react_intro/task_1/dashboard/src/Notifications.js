e56b4fba9e1bf89dc317c9768258ca73d755a5de
Sofia Cheung <sofiavictoria17@gmail.com>
2020-08-18 15:52:07 -0700

    Uploading files

---
+++ 0x02-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Notifications.css';
+
+function notifications() {
+  return (<div className="Notifications">Here is the list of notifications</div>)
+}
+
+export default notifications();
+
