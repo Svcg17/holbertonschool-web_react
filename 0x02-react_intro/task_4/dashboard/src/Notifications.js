d06d36453a4d31262e5132c55fb7f720b2fc2848
Sofia Cheung <sofiavictoria17@gmail.com>
2020-08-18 21:49:27 -0700

    Deploying to github pages

---
+++ 0x02-react_intro/task_4/dashboard/src/Notifications.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Notifications.css';
+import * as Utils from './utils';
+
+function Notifications() {
+  return (
+      <div className="Notifications">
+        <button style={{"aria-label": "Close"}} onClick={console.log('Close button has been clicked')}>x</button>
+        <p>Here is the list of notifications</p>
+        <ul>
+          <li data="default">New course available</li>
+          <li data="urgent">New resume available</li>
+          <li data="urgent" dangerouslySetInnerHTML={{ __html: Utils.getLatestNotification() }}></li>
+        </ul>
+      </div>
+  );
+}
+
+export default Notifications();
+
