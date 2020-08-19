1600c1b61917224befa241179f48d616f5f6e9ea
Sofia Cheung <sofiavictoria17@gmail.com>
2020-08-18 21:48:22 -0700

    Updates

---
+++ precache-manifest.54b301a56ffab00f33a33a151ae62869.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9641d6744fe01d3663d952b90a207ad3",
+    "url": "/holbertonschool-web_react/0x02-react_intro/task_4/dashboard/index.html"
+  },
+  {
+    "revision": "aaaf465c3e973fbe842f",
+    "url": "/holbertonschool-web_react/0x02-react_intro/task_4/dashboard/static/css/main.b076f8db.chunk.css"
+  },
+  {
+    "revision": "43175e84fdfafc547eb2",
+    "url": "/holbertonschool-web_react/0x02-react_intro/task_4/dashboard/static/js/2.1baaf7bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/holbertonschool-web_react/0x02-react_intro/task_4/dashboard/static/js/2.1baaf7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaaf465c3e973fbe842f",
+    "url": "/holbertonschool-web_react/0x02-react_intro/task_4/dashboard/static/js/main.dd2ee8aa.chunk.js"
+  },
+  {
+    "revision": "a75a1a0f5e694ad55019",
+    "url": "/holbertonschool-web_react/0x02-react_intro/task_4/dashboard/static/js/runtime-main.9933cb2d.js"
+  },
+  {
+    "revision": "23d20eb3253de8404d66401eb6db58d5",
+    "url": "/holbertonschool-web_react/0x02-react_intro/task_4/dashboard/static/media/holberton-logo.23d20eb3.jpg"
+  }
+]);
